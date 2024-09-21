6d931d7a0ca1aa4993be38cc6d10791fb6ec3c31
Lucas Manoel <72095755+lucsmanoel@users.noreply.github.com>
2024-09-21 14:58:53 -0300

    atualizações

--- EstruturaDeDados/aula03_lista_ligada/Programa.java
+++
@@ -1,9 +0,0 @@
-package aula03_lista_ligada;
-
-public class Programa {
-    public static void main(String[] args) {
-        ListaLigada lista = new ListaLigada();
-        lista.adicionar("AC");
-        System.out.println( "TAMANHO: " + lista.getTamanho());
-    }
-}
